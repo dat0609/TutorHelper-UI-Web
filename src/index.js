aabc6a1c425f3ef7126fd0616a892e0b5d8dea1d
CongThanh <pdcthanh112@gmail.com>
2021-09-27 21:11:50 +0700

    update template

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
+/* eslint-disable import/default */
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+require('./favicon.ico');
+import './styles.scss';
+import 'font-awesome/css/font-awesome.css';
+import 'flexboxgrid/css/flexboxgrid.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+injectTapEventPlugin();
+
+render(
+    <Router routes={routes} history={browserHistory} />, document.getElementById('app')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
